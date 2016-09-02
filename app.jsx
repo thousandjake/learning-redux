14a4ff2f1454df92ef0134f4df30c383c3f9272d
Jacob Thousand <thousandjake@gmail.com>
2016-09-02 15:03:44 -0500

    added input and addtodo button

--- app.jsx
+++ app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from 'react';
 import { combineReducers } from 'redux';
 import { createStore } from 'redux';
 
@@ -57,44 +58,45 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp);
 
-console.log('Initial State:');
-console.log(store.getState());
-console.log('--------------');
+let nextTodoId = 0;
 
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux'
-});
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
+class TodoApp extends Component {
+  render () {
+    return (
+      <div>
+        <input ref={node => {
+            this.input = node;
+          }} />
+        <button onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: nextTodoId++
+            });
+            this.input.value = '';
+          }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go Shopping'
-});
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
+const render = () => {
+  ReactDOM.render(
+    <TodoApp
+      todos={store.getState().todos}
+    />,
+    document.getElementById('root')
+  );
+};
 
-console.log('Dispatching TOGGLE_TODO');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0,
-});
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED'
-});
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
+store.subscribe(render);
+render();
